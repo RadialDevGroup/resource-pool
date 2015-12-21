daaa90198c0c4a70e2cbc4777c8447bf7e46c0ee
pairing <pairing@ZEUS>
2015-12-21 15:16:23 -0700

    Added ability to list candidates Added ability to see individual candidates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,13 @@
 class CandidatesController < ApplicationController
+
+  def index
+    @candidates = Candidate.all
+  end
+
+  def show
+    @candidate = Candidate.find(params[:id])
+  end
+  
   def new
     @candidate = Candidate.new
   end
---
+++ spec/features/viewing_candidates_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Viewing the list of candidates' do
+  let!(:candidates) { create_list :candidate, 3  }
+  let(:candidate) { candidates.sample }
+  let(:user) { create :user, password: 'password', confirmed_at: Time.now }
+
+  before do
+    sign_in user
+  end
+
+  scenario "a user can see a list of potential candidates" do
+    visit candidates_path
+    
+    expect(page).to have_content candidates.first.name
+    expect(page).to have_content candidates.first.email
+    expect(page).to have_content candidates.first.telephone
+  end
+
+  scenario "someone views an individual candidate" do
+    visit candidates_path
+    
+    click_link candidate.name, candidate_path(candidate.id)
+    expect(page).to have_content candidate.name
+    expect(page).to have_content candidate.email
+    expect(page).to have_content candidate.telephone
+    expect(page).to have_content candidate.linkedin
+    expect(page).to have_content candidate.twitter
+    expect(page).to have_content candidate.referral_source
+  end
+end
